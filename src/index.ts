a891fd15c8cc214a004ed0ab20a8b90b4f20031b
ericchase <github@eric.addymail.com>
2024-09-17 11:03:49 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { a, G, x, y } from "./src/this part doesn't even matter";
+
+
+
+// your cursor must be between code. placing the cursor at the
+// end of the file or at the beginning will not trigger the
+// behavior
+
+
+
+
+
+
+
+
+a;
+G;
+y;
+x;
